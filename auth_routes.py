268a1eccecde9496c3b26ce9049fc483e84fab6e
John201ab <jh5777720@gmai.com>
2026-07-18 18:58:15 -0700

    criação de usuarios integrado com banco de dados criado com sucesso!

--- auth_routes.py
+++ auth_routes.py
@@ -1,7 +1,28 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, HTTPException
+from models import Usuario
+from dependencies import pegar_sessao
+from main import bcrypt_context
+from schemas import UsuarioSchemas
+from sqlalchemy.orm import Session
 
 auth_router = APIRouter(prefix="/auth", tags=["auth"])
 
 @auth_router.get("/lista")
-async def autenticar():
+async def home():
     return {"mensagem":"Rota autenticação pedidos acessada!"}
+
+@auth_router.post("/criar_conta")
+async def criar_conta(usuarioschemas: UsuarioSchemas, session: Session = Depends(pegar_sessao)):
+    usuario = session.query(Usuario).filter(Usuario.email==usuarioschemas.email).first()
+    if usuario:
+        raise HTTPException(status_code=400, detail="e-mail já cadastrado")
+    else:
+        print(senha)
+        senha_criptografada = bcrypt_context.hash(usuarioschemas.senha)
+        novo_usuario = Usuario(usuarioschemas.nome, usuarioschemas.email, senha_criptografada)
+    
+        session.add(novo_usuario)
+        session.commit()
+        session.close()
+        return{"mensagem":f"e-mail {usuarioschemas.email} cadastrado com sucesso!!"}
+        
